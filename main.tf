3de74c16c9cd58d9b4eeac94f29bda943eb5192c
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-09-10 01:41:55 +0200

    add crn job

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 resource "google_pubsub_topic" "topic" {
   name = "instance-scheduler-topic"
 }
+
+resource "google_cloud_scheduler_job" "cr_job" {
+  name        = "instance-scheduler"
+  description = "Cloud Scheduler to turn off labeled VMs."
+  schedule    = var.cron_pattern
+
+  pubsub_target {
+    topic_name = google_pubsub_topic.topic.id
+    data       = base64encode("foo, bar..")
+  }
+}
